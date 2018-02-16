4b2b4c400d5745d1508c12684c9adcf8ad54293c
Krissda Prakalphakul <krissdap@hotmail.com>
2018-02-16 16:54:53 +0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # set-proxy-for-ubuntu-server
 Shell script for configuring proxy for ubuntu server linux. Because some applications read proxy setting from their configuration file, not from global variables.
+
+# Usage
+```sh
+sudo set_proxy.sh
+```
+
+# About
+I wrote this script to fix annoyance of setting proxy configuration every time I deployed a new Ubuntu Linux server behind a proxy.
+The types of configuration supported depends on my need of course.
+So feel free to copy/fork/modify it however you need or like.
+
+If you plan to use this script, it probably means you're behind some sort of proxy and probably can't reach github to get it, chicken and egg problem. :-)
+So again feel free to copy/distribute it however you need or like.
---
+++ set_proxy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Step 1: Get proxy server configuration. Proxy name (or IP), username (optional), and password (optional)
+echo "Set proxy server for apt, wget, and more"
+echo "Please enter your proxy url"
+read proxy
+echo "Please enter username for proxy (leave as blank if no authentication needed)"
+read username
+if [ -n "$username" ]
+then
+	echo "Please enter password for proxy (leave as blank if no password needed)"
+	read -s password
+	esc_username=${username/@/%40}
+fi
+
+# Step 2: Set configuration for each program
+
+# http_proxy variable in /etc/environment
+echo http_proxy="http://$esc_username:$password@$proxy" | sudo tee -a /etc/environment
+
+# apt (apt-get)
+apt_conf='/etc/apt/apt.conf'
+if [ -f $apt_conf ]
+then
+	if [ -z "$username" ]
+	then
+		proxy_string='Acquire::http::Proxy "http://'$proxy'";'
+	else
+		if [ -z "$password" ]
+		then
+			proxy_string='Acquire::http::Proxy "http://'$esc_username'@'$proxy'";'
+		else
+			proxy_string='Acquire::http::Proxy "http://'$esc_username':'$password'@'$proxy'";'
+		fi
+	fi
+	# echo $proxy_string
+	echo "apt.conf exist, checking for proxy configuration"
+	existing_proxy_lines=`grep "^Acquire::http::Proxy" $apt_conf`
+	find_count=`echo $existing_proxy_lines | wc -l`
+	if [ -z "$existing_proxy_lines" ]
+	then
+		echo sending proxy string $proxy_string to $apt_conf
+		echo $proxy_string >> $apt_conf
+	else
+		echo "Existing proxy configuration"
+		echo $existing_proxy_lines
+		# sed_parameter="'s#$existing_proxy_lines#$proxy_string#'"
+		exec_sed() {
+			# echo sed -i "s#$1#$2#" $3
+			sudo sed -i "s#$1#$2#" $3
+		}
+		echo "New proxy configuration"
+		# echo $proxy_string
+		exec_sed "$existing_proxy_lines" "$proxy_string" "$apt_conf"
+	fi
+else
+	echo "apt.conf not exists, creating new file"
+	echo $proxy_string > $apt_conf
+fi
+
+# git
+git_proxy_string='http://'$esc_username':'$password'@'$proxy
+git config --global http.proxy $git_proxy_string
+
